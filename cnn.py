6581a979297a7ed315e818cc8b2535d9ccdb674d
Frederic Renard <fredericrenard99@gmail.com>
2023-04-14 13:27:48 +0000

    VGG with dropout implemented - first training with accuracy > .60 but training is very slow. Investigate higher learning rates and more epochs. Added stopping if performance is above a certain number

--- cnn.py
+++ cnn.py
@@ -14,12 +14,74 @@ class VGG(nn.Module):
 
         # convolutional layers
         if dropout and not batchnorm:
-            pass
-        
+            print("VGG model with dropout")
+            self.conv_layers = nn.Sequential(
+
+            # First VGG block
+            nn.Conv2d(in_channels=self.in_channels,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                     padding="same"),
+            nn.ReLU(),  
+            nn.Conv2d(in_channels=32,
+                      out_channels=32,
+                      kernel_size=3,
+                      stride=1,
+                     padding="same"),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=2),  
+            nn.Dropout(0.2),
+
+            # Second VGG block
+            nn.Conv2d(in_channels=32,
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                     padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=3,
+                      stride=1,
+                     padding="same"),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.2),
+
+            # Third VGG block
+            nn.Conv2d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=3,
+                     padding="same"),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=3,
+                     padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.2),
+
+        )
+                
+        # fully connected linear layers
+            self.linear_layers = nn.Sequential(
+            nn.Linear(in_features=128 * 4 * 4, out_features=128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(in_features=128, out_features=self.num_classes),
+            nn.Softmax()
+        )        
+            
         if batchnorm and not dropout:
+            print("VGG model with batchnorm")
+
             pass
         
         if batchnorm and dropout:
+            print("VGG model with dropout and batchnorm")
+
             pass
         
         else:
@@ -75,7 +137,7 @@ class VGG(nn.Module):
             nn.Linear(in_features=128 * 4 * 4, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=self.num_classes),
-            nn.Softmax()
+            nn.Softmax(dim=0)
         )
 
     def forward(self, x):
@@ -91,13 +153,14 @@ class VGG(nn.Module):
         self.linear_layers.bias.data.fill_(0.01)
 
 class Trainer:
-    def __init__(self, model, train_dataloader, val_dataloader, criterion, optimizer, device, log_dir):
+    def __init__(self, model, train_dataloader, val_dataloader, criterion, optimizer, device, log_dir, obj_performance):
         self.model = model
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
+        self.obj_performance = obj_performance
         self.writer = SummaryWriter(log_dir=f"logs/" + log_dir + f"{datetime.now().strftime('%Y%m%d-%H%M%S')}")
 
     def train(self, epochs):
@@ -158,5 +221,7 @@ class Trainer:
             self.writer.add_scalar("Accuracy/val", val_acc, epoch)
 
             print(f"Epoch {epoch+1}/{epochs}: Train Loss = {train_loss:.4f}, Train Accuracy = {train_acc:.4f}, Val Loss = {val_loss:.4f}, Val Accuracy = {val_acc:.4f}")
-        
+            
+            if val_acc >= self.obj_performance:
+                print("Solved")
         return train_loss_list, train_acc_list, val_loss_list, val_acc_list
